5c38028c5d5a147222980644ac26c1c49b8a42e9
peakwiny <peakwiny@gmail.com>
2016-10-17 15:37:24 +0700

    phpbbbb

--- index.php
+++ index.php
@@ -13,6 +13,9 @@ and open the template in the editor.
         <?php
         // put your code here
         echo 'Hello php peeraphat';
+        echo '<hr>';
+        $srr=['delphi','php','java','javascrip'];
+        print_r($srr);
         ?>
     </body>
 </html>
